ef0f21bb58b03b065e5e2dac330dcb0ec7d5b801
Gold Edem Hogan <edge37@outlook.com>
2025-07-16 08:45:26 +0100

    Add new quiz

--- index.js
+++ index.js
@@ -289,6 +289,8 @@ async function createEndOfTermQuiz({ t, selectedClass, subject, grade }) {
         \`\`\`
 
  let the questions be numbered.
+ let the questions be extremely easy.
+ let the questions be extremely to answer.
         sections may have subsections, with headings, instructions for the questions that follow perhaps, or passages, or just such parts that are not really questions in themselves, e.g "Write the short form of the following words". Add such parts as unnumbered questions, except for mainsections A, B and C.
         ${extra_instructions}
 
